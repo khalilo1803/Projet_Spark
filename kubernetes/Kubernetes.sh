cca4f7bb70d726315228b9c9185ef465cfea2d75
khalilo1803 <47722569+khalilo1803@users.noreply.github.com>
2023-01-18 20:21:28 +0100

    script 3
    khalil+ala

---
+++ kubernetes/Kubernetes.sh
@@ -0,0 +1,28 @@
+#!bin/bash
+basedir = $(cd $(dirname $ 0) ; pwd )
+jobName = "Reading a csv file "
+className = org.example.khalil.SparkApp
+utility_jar =  $($basedir/utility_jar)
+app_jar = /spp/target/spp-1.0-SNAPSHOT.jar
+echo "Submitting spark job" 
+mvn install 
+./spark-submit --class   $className 
+--name $jobName
+--conf  spar.driver.memory= 2g
+--jars $utility_jar
+--master local 
+--files $(basedir/config.properties) 
+$app_jar
+
+docker build -f docker/Dockerfile -t spark-hadoop:3.2.0 ./docker
+kubectl create -f ./kubernetes/spark-master-deployment.yaml
+kubectl create -f ./kubernetes/spark-master-service.yaml
+kubectl create -f ./kubernetes/spark-worker-deployment.yaml
+
+spark-submit 
+  --master k8s://https://kubernetes.docker.internal:6443 
+  --deploy-mode cluster  
+  --class $className
+  --conf spark.executor.instances=5 
+  --conf spark.kubernetes.container.image=newfrontdocker/spark:v3.0.1-j14 
+$app_jar
